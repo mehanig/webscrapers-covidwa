95faf5bc267e4caf65a8cde840c5df84fab2a8ee
mehanig <mehanig@gmail.com>
2021-02-07 03:18:49 +0100

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Basic scraper for COVID19 from Shoreline Clinic and Holly Park Clinic
+
+## How to run:
+
+`npx cross-env API_SECRET={{ secrets.apiKey }} npm run start`
---
+++ src/index.js
@@ -0,0 +1,54 @@
+const axios = require("axios")
+const $ = require('cheerio')
+
+const APIURL = "https://api.covidwa.com/v1/updater";
+
+const NotAvailable = {availability: 0};
+const BrokenState = {availability: 2};
+
+const statusGetter = (name, url) => {
+    return axios.get(url).then((response) => {
+        if (response.status != 200) {
+            console.log(response.status);
+            return BrokenState
+        }
+        try {
+            const h2withName = $('h2', response.data);
+            if (h2withName.text() !== name) {
+                return BrokenState
+            }
+            if (h2withName.next().text().startsWith("Closed")) {
+                return NotAvailable
+            } else {
+                return BrokenState
+            }
+        } catch(error) {
+            console.log(error)
+            return BrokenState
+        }
+    })
+}
+
+const poster = (result, key) => {
+    if (!process.env.API_SECRET) {
+        console.log("Please set API_SECRET. Result not posted.");
+        return;
+    }
+    if (result && result.availability != undefined) {
+        axios.post(APIURL, {
+            key,
+            status: result.availability,
+            secret: process.env.API_SECRET
+        }).then(() => {
+            console.log("Result posted:" + result.availability)
+        }).catch((e) => {
+            console.log("Result not posted. Error:" + e);
+        })
+    } else {
+        console.log("Result not pushed to the api.")
+        console.log(result);
+    }
+}
+
+statusGetter('Holly Park Vaccination Site', 'https://www.solvhealth.com/book-online/0kMrzp').then(result => poster(result, "Holly Park"));
+statusGetter('Shoreline Vaccination Site', 'https://www.solvhealth.com/book-online/pR8j70').then(result => poster(result, "Shoreline"));
